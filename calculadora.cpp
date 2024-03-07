3e046f21c8680d16e85dda7f76799e07526b5a70
15Tano <162596334+15Tano@users.noreply.github.com>
2024-03-07 08:38:03 -0600

    Update calculadora.cpp
    Ya

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,60 +1,68 @@
 #include <iostream>
+#include <cmath> // Necesario para la función sqrt
 using namespace std;
 
-struct Calculadora{
+struct Calculadora {
+    Calculadora() {}
 
-    Calculadora (){
-
-    }
-    void pedir_num (float sec_num[], int lim){
+    void pedir_num(float sec_num[], int lim) {
         int x;
-        for (x=0 ; x < lim ; x++){
+        for (x = 0; x < lim; x++) {
             cin >> sec_num[x];
         }
     }
-    float suma (float sec_num[], int lim) {
+
+    float suma(float sec_num[], int lim) {
         float sum = 0.0;
         int x;
-        for (x=0 ; x < lim ; x++){
+        for (x = 0; x < lim; x++) {
             sum += sec_num[x];
         }
         return sum;
     }
-    float resta (float sec_num[], int lim) {
+
+    float resta(float sec_num[], int lim) {
         float res = sec_num[0];
         int x;
-        for (x=1 ; x < lim ; x++){
+        for (x = 1; x < lim; x++) {
             res -= sec_num[x];
         }
         return res;
     }
-    float multiplicacion (float sec_num[], int lim){
+
+    float multiplicacion(float sec_num[], int lim) {
         float multi = 1.0;
         int x;
-        for (x=0 ; x < lim ; x++){
+        for (x = 0; x < lim; x++) {
             multi *= sec_num[x];
         }
         return multi;
     }
-    float division (float sec_num[]){
+
+    float division(float sec_num[]) {
         float divi;
         divi = sec_num[0] / sec_num[1];
         return divi;
     }
+
+    float raizCuadrada(float num) {
+        return sqrt(num);
+    }
 };
 
-int main (){
+int main() {
     struct Calculadora calculadora1;
-    float sec_num [100];
-    int lim,op;
+    float sec_num[100];
+    int lim, op;
 
-    do{
+    do {
         cout << "---> Menu <---" << endl;
-        cout << "1.Sumar" << endl;
-        cout << "2.Restar" << endl;
-        cout << "3.Multiplicacion" << endl;
-        cout << "4.Division" << endl;
-        cout << "5.Salir" << endl;
+        cout << "1. Sumar" << endl;
+        cout << "2. Restar" << endl;
+        cout << "3. Multiplicacion" << endl;
+        cout << "4. Division" << endl;
+        cout << "5. Raiz Cuadrada" << endl;
+        cout << "6. Salir" << endl;
         cin >> op;
 
         switch (op) {
@@ -62,33 +70,40 @@ int main (){
                 cout << "Ingresa cantidad de numeros a sumar" << endl;
                 cin >> lim;
                 cout << "Ingrese los numeros: " << endl;
-                calculadora1.pedir_num(sec_num , lim);
-                cout << "La suma es: " << calculadora1.suma (sec_num , lim) << endl;
-            break;
+                calculadora1.pedir_num(sec_num, lim);
+                cout << "La suma es: " << calculadora1.suma(sec_num, lim) << endl;
+                break;
             case 2:
                 cout << "Ingressa la cantidad de numeros a restar" << endl;
                 cin >> lim;
                 cout << "Ingrese los numeros: " << endl;
-                calculadora1.pedir_num(sec_num , lim);
-                cout << "La resta es: " << calculadora1.resta(sec_num , lim) << endl;
-            break;
+                calculadora1.pedir_num(sec_num, lim);
+                cout << "La resta es: " << calculadora1.resta(sec_num, lim) << endl;
+                break;
             case 3:
-                cout << "Ingressa la cantidad de numeros a mulyiplicar" << endl;
+                cout << "Ingressa la cantidad de numeros a multiplicar" << endl;
                 cin >> lim;
                 cout << "Ingrese los numeros: " << endl;
-                calculadora1.pedir_num(sec_num , lim);
-                cout << "La multiplicacion es: " << calculadora1.multiplicacion(sec_num , lim) << endl;
-            break;
+                calculadora1.pedir_num(sec_num, lim);
+                cout << "La multiplicacion es: " << calculadora1.multiplicacion(sec_num, lim) << endl;
+                break;
             case 4:
                 cout << "Ingressa 2 numeros a dividir" << endl;
-                calculadora1.pedir_num(sec_num , 2);
+                calculadora1.pedir_num(sec_num, 2);
                 cout << "La division es: " << calculadora1.division(sec_num) << endl;
-            break;
+                break;
             case 5:
+                cout << "Ingressa el numero para calcular la raiz cuadrada: ";
+                cin >> sec_num[0];
+                cout << "La raiz cuadrada es: " << calculadora1.raizCuadrada(sec_num[0]) << endl;
+                break;
+            case 6:
                 cout << "Bye" << endl;
-            break;
-            default: cout << "Opcion invalida";
+                break;
+            default:
+                cout << "Opcion invalida";
         }
-    }while (op != 5);
+    } while (op != 6);
+
     return 0;
 }
